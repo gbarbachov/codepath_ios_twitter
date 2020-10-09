c61c78c0fa496c6eab7fc395723ddf7a556b2053
grigori <gbarbachov@bktaxcpa.com>
2020-10-09 02:11:36 -0700

    "Stay Logged In" functionality is completed

--- Twitter/LoginViewController.swift
+++ Twitter/LoginViewController.swift
@@ -9,10 +9,17 @@
 import UIKit
 
 class LoginViewController: UIViewController {
+    
+    override func viewDidAppear(_ animated: Bool) {
+        if UserDefaults.standard.bool(forKey: "userLoggedIn") == true{
+            self.performSegue(withIdentifier: "loginToHome", sender: self)
+        }
+    }
 
     @IBAction func onButtonLogin(_ sender: Any) {
         let twitterUrl = "https://api.twitter.com/oauth/request_token"
         TwitterAPICaller.client?.login(url: twitterUrl, success: {
+            UserDefaults.standard.set(true, forKey: "userLoggedIn")
             self.performSegue(withIdentifier: "loginToHome", sender: self)
         }, failure: { (Error) in
             print("Not logged in")
