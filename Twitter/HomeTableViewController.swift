48f2f2e24ff920a43ac58d6e8127f6c8990edd95
grigori <gbarbachov@bktaxcpa.com>
2020-10-16 21:32:11 -0700

    favoriteTweet and retweet functions added

--- Twitter/HomeTableViewController.swift
+++ Twitter/HomeTableViewController.swift
@@ -29,8 +29,9 @@ class HomeTableViewController: UITableViewController {
         self.tableView.estimatedRowHeight = 150
     }
     
-    override func viewDidAppear(_ animated: Bool){           super.viewDidAppear(animated)
-           self.loadTweets()
+    override func viewDidAppear(_ animated: Bool){
+        super.viewDidAppear(animated)
+        self.loadTweets()
     }
     @objc func loadTweets(){
         numberOfTweets = 20
@@ -90,6 +91,10 @@ class HomeTableViewController: UITableViewController {
         if let imageData = data{
             cell.profileImageView.image = UIImage(data: imageData)
         }
+        cell.setFavorite(tweetArray[indexPath.row]["favorited"] as! Bool)
+        cell.tweetId = tweetArray[indexPath.row]["id"] as! Int
+        cell.setRetweeted(tweetArray[indexPath.row]["retweeted"] as! Bool)
+             
         return cell
     }
     // MARK: - Table view data source
@@ -104,59 +109,5 @@ class HomeTableViewController: UITableViewController {
         return tweetArray.count
     }
 
-    /*
-    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
-
-        // Configure the cell...
-
-        return cell
-    }
-    */
-
-    /*
-    // Override to support conditional editing of the table view.
-    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the specified item to be editable.
-        return true
-    }
-    */
-
-    /*
-    // Override to support editing the table view.
-    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
-        if editingStyle == .delete {
-            // Delete the row from the data source
-            tableView.deleteRows(at: [indexPath], with: .fade)
-        } else if editingStyle == .insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
-    }
-    */
-
-    /*
-    // Override to support rearranging the table view.
-    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
-
-    }
-    */
-
-    /*
-    // Override to support conditional rearranging of the table view.
-    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the item to be re-orderable.
-        return true
-    }
-    */
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
-}
+        }
+  
--- Twitter/TweetCellTableViewCell.swift
+++ Twitter/TweetCellTableViewCell.swift
@@ -13,6 +13,63 @@ class TweetCellTableViewCell: UITableViewCell {
     @IBOutlet weak var tweetContent: UILabel!
     @IBOutlet weak var userNameLabel: UILabel!
     @IBOutlet weak var profileImageView: UIImageView!
+    @IBOutlet weak var favorButton: UIButton!
+    @IBOutlet weak var retweetButton: UIButton!
+    
+    var favorited:Bool = false
+    var tweetId: Int = -1
+
+    
+    @IBAction func favoriteTweet(_ sender: Any) {
+        let toBeFavorited = !favorited
+            if(toBeFavorited){
+                    TwitterAPICaller.client?.favoriteTweet(tweetId: tweetId, success: {
+                        self.setFavorite(true)
+                    }, failure: { (error) in
+                        print("Favorite method did not succeed: \(error)")
+                    })
+                }
+            else {
+                        TwitterAPICaller.client?.favoriteTweet(tweetId: tweetId, success: {
+                            self.setFavorite(false)
+                        }, failure: { (error) in
+                            print("Unfavorite method did not succeed: \(error)")
+                        })
+                }
+
+        
+    }
+    
+    
+    func setFavorite(_ isFavorited: Bool) {
+            favorited = isFavorited
+            if(favorited){
+                favorButton.setImage(UIImage(named:"favor-icon-red"), for: UIControl.State.normal)
+                }
+            else{
+                favorButton.setImage(UIImage(named:"favor-icon-1"), for: UIControl.State.normal)
+            }
+        }
+    
+    @IBAction func retweet(_ sender: Any) {
+        TwitterAPICaller.client?.retweet(tweetId: tweetId, success: {
+                    self.setRetweeted(true)
+                }, failure: { (error) in
+                    print("Error retweeting: \(error)")
+                })
+
+    }
+       
+    func setRetweeted(_ isRetweeted: Bool) {
+           if(isRetweeted){
+               retweetButton.setImage(UIImage(named:"retweet-icon-green"), for: UIControl.State.normal)
+               retweetButton.isEnabled = false
+           }
+           else{
+               retweetButton.setImage(UIImage(named:"retweet-icon"), for: UIControl.State.normal)
+               retweetButton.isEnabled = true
+           }
+       }
     override func awakeFromNib() {
         super.awakeFromNib()
         // Initialization code
